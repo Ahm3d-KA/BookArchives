8a8cf0ebbb13f97bf5591c741ece1ce567e9d58c
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-07-03 16:31:39 +0100

    add more to table and sort typography

---
+++ BookArchives/Controllers/ExploreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookArchives.Controllers;
+
+public class ExploreController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- BookArchives/tailwind.config.js
+++ BookArchives/tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   daisyui: {
     themes: ["nord"]
   },
-  plugins: [require("daisyui")]
+  plugins: [require("@tailwindcss/typography"), require("daisyui")]
 };
