5c42d576e8fb0eb699994388b1b3a4a0666858cd
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-07-03 16:04:23 +0100

    create my books table

---
+++ BookArchives/Controllers/MyBooksController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using BookArchives.Data;
+using BookArchives.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookArchives.Controllers;
+
+public class MyBooksController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public MyBooksController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        IEnumerable<UserBooksModel> objUserBooksList = _db.ArchiveDb;
+        return View(objUserBooksList);
+    }
+}
